2ec38bcad5bc52bd2a26134eba03619a9f2a4c7b
Andrei Shved <svensk.tut.by@gmail.com>
2022-07-12 21:12:36 +0300

    feat: add handling fetch error

--- src/Films.js
+++ src/Films.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { useQuery } from 'react-query';
 
 const Films = () => {
-    const { data: { results = [] } = {}, isLoading } = useQuery('key', () => {
+    const {
+        data: { results = [] } = {},
+        isLoading,
+        isError,
+        error,
+    } = useQuery('key', () => {
         return fetch('http://swapi.dev/api/films').then((res) => res.json());
     });
 
+    if (isError) return <div>{error.message}</div>;
     if (isLoading) return <div>Loading...</div>;
 
     return (
