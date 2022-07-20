49bbb7da3be4cd08e47ac319705c30bd13b770ab
Andrei Shved <svensk.tut.by@gmail.com>
2022-07-20 22:00:19 +0300

    feat: add custom react-query useGetPlanets hook

--- src/Films.js
+++ src/Films.js
@@ -6,6 +6,11 @@ const useGetFilms = (queryKey = 'films') =>
         return fetch('http://swapi.dev/api/films').then((res) => res.json());
     });
 
+const useGetPlanets = (queryKey = 'planets') =>
+    useQuery(queryKey, () => {
+        return fetch('http://swapi.dev/api/planets').then((res) => res.json());
+    });
+
 const FilmsLength = ({ queryKey }) => {
     const { data: { results = [] } = {}, isLoading } = useGetFilms(queryKey);
 
@@ -27,6 +32,25 @@ const Films = ({ queryKey }) => {
             <ul>
                 {results?.map(({ title, episode_id }) => (
                     <li key={episode_id}>{title}</li>
+                ))}
+            </ul>
+            <Planets />
+        </>
+    );
+};
+
+const Planets = ({ queryKey }) => {
+    const { data: { results = [] } = {}, isLoading, isFetching, isError, error } = useGetPlanets(queryKey);
+
+    if (isError) return <div>{error.message}</div>;
+    if (isLoading) return <div>Loading...</div>;
+    if (isFetching) return <div>Fetching...</div>;
+
+    return (
+        <>
+            <ul>
+                {results?.map(({ name }) => (
+                    <li key={name}>{name}</li>
                 ))}
             </ul>
         </>
