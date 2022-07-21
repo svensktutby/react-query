0c3857ef4bb517af2ac3d33890339f7056d2ef7b
Andrei Shved <svensk.tut.by@gmail.com>
2022-07-21 21:36:23 +0300

    feat: add live search with dynamically query key

--- src/Films.js
+++ src/Films.js
@@ -1,26 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 
-const useGetFilms = (queryKey = 'films') =>
-    useQuery(queryKey, () => {
-        return fetch('http://swapi.dev/api/films').then((res) => res.json());
+const useGetFilm = (film) =>
+    useQuery(film, () => {
+        return fetch(`http://swapi.dev/api/films?&search=${film}`).then((res) => res.json());
     });
 
-const useGetPlanets = (queryKey = 'planets') =>
-    useQuery(queryKey, () => {
-        return fetch('http://swapi.dev/api/planets').then((res) => res.json());
-    });
-
-const FilmsLength = ({ queryKey }) => {
-    const { data: { results = [] } = {}, isLoading } = useGetFilms(queryKey);
-
-    if (isLoading) return <div>Loading...</div>;
-
-    return <p>Films length: {results?.length}</p>;
-};
-
-const Films = ({ queryKey }) => {
-    const { data: { results = [] } = {}, isLoading, isFetching, isError, error } = useGetFilms(queryKey);
+const SearchFilm = ({ film }) => {
+    const { data: { results = [] } = {}, isLoading, isFetching, isError, error } = useGetFilm(film);
 
     if (isError) return <div>{error.message}</div>;
     if (isLoading) return <div>Loading...</div>;
@@ -28,31 +15,22 @@ const Films = ({ queryKey }) => {
 
     return (
         <>
-            <FilmsLength queryKey="filmsLength" />
             <ul>
                 {results?.map(({ title, episode_id }) => (
                     <li key={episode_id}>{title}</li>
                 ))}
             </ul>
-            <Planets />
         </>
     );
 };
 
-const Planets = ({ queryKey }) => {
-    const { data: { results = [] } = {}, isLoading, isFetching, isError, error } = useGetPlanets(queryKey);
-
-    if (isError) return <div>{error.message}</div>;
-    if (isLoading) return <div>Loading...</div>;
-    if (isFetching) return <div>Fetching...</div>;
+const Films = () => {
+    const [film, setFilm] = useState('');
 
     return (
         <>
-            <ul>
-                {results?.map(({ name }) => (
-                    <li key={name}>{name}</li>
-                ))}
-            </ul>
+            <input type="text" value={film} onChange={({ target: { value } }) => setFilm(value)} />
+            <SearchFilm film={film} />
         </>
     );
 };
